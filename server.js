83dfe334802492bd0e09aa15ed8440f6e3b74ad7
anji862 <singh45anjali@gmail.com>
2021-10-09 02:34:19 +0530

    enough

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express')
+const connectDb = require('./backend/config/db')
+const productRoutes = require('./backend/routes/productRoutes')
+const path=require('path')
+
+connectDb();
+const app = express();
+
+app.use(express.json());
+app.use('/api/products',productRoutes)
+
+const PORT = process.env.PORT || 5000;
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('/frontend/build'));
+}
+
+app.listen(PORT, () => console.log("Server is running"))
