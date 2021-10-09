782d6b13495c29544a922f54cf467d77d7f810e8
anji862 <singh45anjali@gmail.com>
2021-10-09 15:08:32 +0530

    iwil

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('/frontend/build'));
 }
 
-app.get("*", function (request, response) {
+app.get("/*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
 });
 
